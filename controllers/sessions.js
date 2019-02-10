3665e180b9efff9fca3c7341f5b8dc8ff3ebaf40
Joe Hinkley <joehhinkley@gmail.com>
2019-02-09 18:59:23 -0800

    added check for incorrect password in session post route

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -4,16 +4,29 @@ const User = require('../models/users.js');
 const bcrypt = require('bcrypt');
 
 router.post('/', (req, res) => {
+  // Find user with provided username
   User.findOne({username: req.body.username}, (err, foundUser) => {
-      if (!foundUser){
-        res.status(401).json({
-          status: 401,
-          message: 'invalid'
-        })
-      } else if(bcrypt.compareSync(req.body.password, foundUser.password)){
-        req.session.currentUser = foundUser;
-        res.status(201).json(foundUser)
-      }
+    // If there is no user with that username
+    if (!foundUser){
+      // Send error to client
+      res.status(409).json({
+        status: 409,
+        message: 'Username does not exist'
+      })
+    } else if(bcrypt.compareSync(req.body.password, foundUser.password)){
+      // provided password matches user password
+      // save found user as currentUser
+      req.session.currentUser = foundUser;
+
+      // Send user object back to client
+      res.status(201).json(foundUser)
+    } else {
+      // Send error to client
+      res.status(401).json({
+        status: 401,
+        message: 'Incorrect password'
+      })
+    }
   })
 })
 
